ae20e46f725a2edf1c5195568bdd86b039d50d7d
Matheus Silva <matheusuniversal@yahoo.com.br>
2023-02-12 22:04:08 -0300

    Demo api arquivos

---
+++ Arquivos.API/Controllers/ArquivosController.cs
@@ -0,0 +1,24 @@
+﻿using Arquivos.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arquivos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArquivosController: ControllerBase
+    {
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> GetFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("Arquivo nao recebido");
+            var arquivoService = new ArquivoService();
+
+            var result = await arquivoService.SaveFile(file);
+            if (!result)
+                return BadRequest(new {Message= "Falha ao Salvar o Arquivo"});
+            return Ok(new {Message = "Arquivo Salvo com Sucesso"});
+        }
+    }
+}
---
+++ Arquivos.API/Services/ArquivoService.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace Arquivos.API.Services
+{
+    public class ArquivoService
+    {
+        private readonly string _path = @"C:\";
+
+        public async Task<bool> SaveFile(IFormFile file)
+        {
+            try
+            {
+                var PastaDestino = Path.Combine(_path, "imagens");
+                var destino = Path.Combine(PastaDestino, file.FileName);
+                if (!Directory.Exists(PastaDestino))
+                    Directory.CreateDirectory(PastaDestino);
+                if(File.Exists(destino))
+                    File.Delete(destino);
+
+                using (var stream = new FileStream(destino, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                return File.Exists(destino);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ;
+            }
+
+        }
+    }
+}
